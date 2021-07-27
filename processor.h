0ca7374169a7df134cfe702dd4437e4a6aa0e412
d_ttang <d_ttang@163.com>
2021-07-28 01:51:23 +0800

    done part2

---
+++ processor.h
@@ -0,0 +1,30 @@
+#ifndef __processor_h
+#define __processor_h
+
+#include "input.h"
+
+typedef enum {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND,
+} MetaCommandResult;
+
+typedef enum {
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT,
+} PrepareResult;
+
+typedef enum {
+    STATEMENT_INSERT,
+    STATEMENT_SELECT,
+} StatementType;
+
+typedef struct Statement {
+    StatementType type;
+} Statement;
+
+MetaCommandResult do_meta_command(InputBuffer* input_buffer);
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement);
+
+void execute_statement(Statement* statement);
+
+#endif
